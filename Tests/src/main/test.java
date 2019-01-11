8691b80d50dd5a6cc320d7a98fd2bd26943218fd
A92939692 <A92939692@BE1DQ778.emea1.cds.t-internal.com>
2019-01-11 11:38:38 +0100

    Logger add Handler/ Formatter

--- Tests/src/main/test.java
+++ Tests/src/main/test.java
@@ -1,14 +1,21 @@
 package main;
 
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 
 public class test {
 public static void main(String[] args) {
 	Logger logger = Logger.getLogger(test.class.getName());;
-	logger.setLevel(Level.INFO);
+	logger.setLevel(Level.ALL);
+	ConsoleHandler handler = new ConsoleHandler();
+	handler.setFormatter(new SimpleFormatter());
+	logger.addHandler(handler);
 	logger.info("Hello World!");
+
+
 
 }
 }

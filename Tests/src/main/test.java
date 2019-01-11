c70017c8428e148a5c0c0567894d9145a2a543cb
A92939692 <A92939692@BE1DQ778.emea1.cds.t-internal.com>
2019-01-11 13:26:10 +0100

    Remove Handler and formatter

--- Tests/src/main/test.java
+++ Tests/src/main/test.java
@@ -1,21 +1,14 @@
 package main;
 
-import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 
 public class test {
 public static void main(String[] args) {
 	Logger logger = Logger.getLogger(test.class.getName());;
-	logger.setLevel(Level.ALL);
-	ConsoleHandler handler = new ConsoleHandler();
-	handler.setFormatter(new SimpleFormatter());
-	logger.addHandler(handler);
+	logger.setLevel(Level.INFO);
 	logger.info("Hello World!");
-
-
 
 }
 }

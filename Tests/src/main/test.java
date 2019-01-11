bdf576a4ea839aad74e373c55df1d3dd8c13cfd1
A92939692 <A92939692@BE1DQ778.emea1.cds.t-internal.com>
2019-01-11 13:54:20 +0100

    v.1.1

--- Tests/src/main/test.java
+++ Tests/src/main/test.java
@@ -11,26 +11,20 @@ public class test {
 		logger.info("---------------------------------------------");
 		logger.info("               Test Programm ");
 		logger.info("---------------------------------------------");
-		int limit = 100;
+		int limit = 9999;
 		int zahl;
 		int zaehler;
 		boolean primzahl;
 
 		for (zahl = 2; zahl <= limit; zahl++) {
-
 			primzahl = true;
-
 			for (zaehler = 2; zaehler < Math.sqrt(zahl) + 1; zaehler++) {
 				if (zahl % zaehler == 0) {
-
 					primzahl = false;
 					break;
 				}
-
 			}
-
 			if (primzahl) {
-
 				System.out.println(zahl + " ist eine Primzahl");
 			}
 		}

97ce8cc7b85e622aaa1a7bd603487ae8da65ff3e
A92939692 <A92939692@BE1DQ778.emea1.cds.t-internal.com>
2019-01-11 13:49:28 +0100

    Test Programm

--- Tests/src/main/test.java
+++ Tests/src/main/test.java
@@ -1,6 +1,6 @@
 package main;
 
-import java.util.Scanner;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,9 +12,39 @@ public static void main(String[] args) {
 	logger.info("---------------------------------------------");
 	logger.info("               Test Programm ");
 	logger.info("---------------------------------------------");
-	Scanner scan = new Scanner(System.in);
-	System.out.print("User: ");
-	String test = scan.next();
-	System.out.println("your username is "+test);
+	 int limit = 100;
+     int zahl;      // Zu überprüfende Zahl
+     int zaehler;   // Hilfsvariable (möglicher Teiler von zahl)
+     boolean primzahl; // Hilfsvariable, ob die aktuelle Zahl eine Primzahl ist
+
+     // zahl <= x   ist der zu überprüfende Zahlenraum
+     // Beginn bei 2, weil 1 per Definition keine Primzahl ist
+     for (zahl = 2; zahl <= limit; zahl++) {
+         // solange wir für zahl keinen Teiler finden, gehen wir davon aus,
+         // dass es eine Primzahl ist
+         primzahl = true;
+
+         // zaehler ist ein möglicher Teiler. Mögliche nicht-triviale Teiler
+         // liegen im Bereich 2 bis zahl/2 (abgerundet), wenn x aber Teiler
+         // von Zahl und größer als Wurzel(zahl), ist zahl/x < Wurzel(zahl), 
+         // sodass diese Grenze genügt.
+         for (zaehler = 2; zaehler < Math.sqrt(zahl) + 1; zaehler++) {
+             if (zahl % zaehler == 0) {
+                 // zaehler ist ein nichttrivialer Teiler von zahl und damit
+                 // zahl keine Primzahl. Weitere Teiler müssen nicht geprüft
+                 // werden und damit kann die Schleife abgebrochen werden.
+                 primzahl = false;
+                 break;
+             }
+
+
+         }
+
+         if (primzahl) {
+             // Keine Teiler gefunden -> zahl2 ist eine Primzahl
+             System.out.println(zahl+" ist eine Primzahl");
+         }
+     }
+ }
 }
-}
+
